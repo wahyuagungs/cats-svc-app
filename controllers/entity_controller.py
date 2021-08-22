8255576d87f94ac91d232fed72b0248b57a8eb90
wahyuagungs <wahyuagungs@gmail.com>
2021-08-22 22:32:06 +0700

    add about endpoint

--- controllers/entity_controller.py
+++ controllers/entity_controller.py
@@ -30,4 +30,11 @@ def get_entity_by_name():
     data = Counterpart.query.filter(Counterpart.entity_name.ilike(search)).all()
     return [d.as_dict() for d in data]
 
+@entity_controller.rout('/api/entity/about')
+@wrapper
+def get_about():
+   d = dict()
+   d['status'] = 1
+   d['message'] = 'Alright'
+   return d
 
